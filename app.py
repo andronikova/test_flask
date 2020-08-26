76b3226b48e8d9ce015209a6cb84b0bce6cfc635
Daria <andronikova.daria@gmail.com>
2020-08-26 16:57:20 +0300

    deploy

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ import psycopg2
 
 app = Flask(__name__)
 
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+# SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 # DATABASE_URL=os.system("heroku config:get DATABASE_URL -a flasktest00")
 
 # DATABASE_URL = os.environ['DATABASE_URL']
@@ -16,56 +21,58 @@ app = Flask(__name__)
 # print(os.environ['DATABASE_URL'])
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
-# # app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:assa1221@localhost:5432/names_years"
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or \
+                                        "postgresql://postgres:assa1221@localhost:5432/names_years"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
-#
-#
-# class NY(db.Model):
-#     name = db.Column(db.String(),primary_key=True)
-#     year = db.Column(db.Integer)
-#
-#     def __init__(self, name, year):
-#         self.name = name
-#         self.year = year
-#
-#     def __repr__(self):
-#         return '<User {}>'.format(self.name)
-#
-#
-# @app.route('/', methods=('GET','POST'))
-# def index():
-#     if request.method == 'GET':
-#         datas = NY.query.all()
-#
-#
-#         results = [
-#             {
-#                 "name": dat.name,
-#                 "year": dat.year
-#             } for dat in datas]
-#
-#         return render_template("index.html", dbinfo = results)
-#
-#     if request.method == 'POST':
-#         if request.form.get('name') is not None:
-#             newname = request.form.get('name')
-#         if request.form.get('year') is not None:
-#             newyear = request.form.get('year')
-#
-#         new_in_db = NY(name=newname,year=newyear)
-#         db.session.add(new_in_db)
-#         db.session.commit()
+db.init_app(app)
+migrate.init_app(app, db)
 
 
-        # return redirect("/")
+
+class NY(db.Model):
+    name = db.Column(db.String(),primary_key=True)
+    year = db.Column(db.Integer)
+
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
+
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
 
 
-@app.route('/')
-def hello():
-    return {"hello": "world"}
+@app.route('/', methods=('GET','POST'))
+def index():
+    if request.method == 'GET':
+        datas = NY.query.all()
+
+
+        results = [
+            {
+                "name": dat.name,
+                "year": dat.year
+            } for dat in datas]
+
+        return render_template("index.html", dbinfo = results)
+
+    if request.method == 'POST':
+        if request.form.get('name') is not None:
+            newname = request.form.get('name')
+        if request.form.get('year') is not None:
+            newyear = request.form.get('year')
+
+        new_in_db = NY(name=newname,year=newyear)
+        db.session.add(new_in_db)
+        db.session.commit()
+
+
+        return redirect("/")
+
+
+# @app.route('/')
+# def hello():
+#     return {"hello": "world"}
 
 
 if __name__ == '__main__':
